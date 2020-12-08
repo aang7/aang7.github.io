8ef3e525d0255ac2f84872d7fd99d2101284c73e
aang7 <aang.drummer@gmail.com>
2020-12-07 23:40:47 -0600

    last post modified

--- publish.el
+++ publish.el
@@ -227,14 +227,14 @@ CONTENT: string to add."
       (add-class-to-tag "h1" "uk-h2 uk-panel uk-padding uk-background-secondary uk-light uk-margin-left uk-margin-right")
       (when (and (string-match "posts" filename) (not (string-match "index" filename)))
 	(add-content-before-tag "</main" "
-
+<div>
 <div class='comments uk-card uk-card-default uk-margin-auto'>
   <div id='disqus_thread'></div>
   <script>
-    var disqus_config = function () {
+    /*var disqus_config = function () {
     this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
     this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-    };
+    };*/
 
     (function() { // DON'T EDIT BELOW THIS LINE
     var d = document, s = d.createElement('script');
@@ -244,6 +244,7 @@ CONTENT: string to add."
     })();
   </script>
   <noscript>Please enable JavaScript to view the <a href='https://disqus.com/?ref_noscript'>comments powered by Disqus.</a></noscript>
+</div>
 </div>
 "))
       
